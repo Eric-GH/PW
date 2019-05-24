505e82136b101f2419a7b5fd8c2c113045ebf328
Li,Hao <ericworkuse@gmail.com>
2019-05-24 11:47:30 -0600

    Finishup the comments and details for LinePane class

--- GetPass/src/View/LinePane.java
+++ GetPass/src/View/LinePane.java
@@ -1,5 +1,11 @@
 package View;
 
+/**
+ * Author: Hao Li
+ * Date: 05/19,2019
+ * The Secondary frame for add new password records
+ * JavaFX
+ */
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
@@ -8,27 +14,38 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 
 public class LinePane extends HBox {
-    public TextArea name_area = new TextArea();
-    public TextArea user_area = new TextArea();
-    public TextArea pass_area = new TextArea();
-    public Button view_delete = new Button("⊝");
+
+    private TextArea name_area = new TextArea();
+    private TextArea user_area = new TextArea();
+    private TextArea pass_area = new TextArea();
+    Button view_delete = new Button("⊝");
 
     /**
      * Construct to set up the elements in LinePane frame
      */
-    public LinePane(){
+    LinePane(){
+        /*
+        Create 2 line
+         */
+        Separator frontSep = new Separator();
+        Separator backSep = new Separator();
+        /*
+        Set up the id for each elements
+         */
         this.setId("LinePane");
         name_area.setId("line_area");
         user_area.setId("line_area");
         pass_area.setId("line_area");
-        user_area.setEditable(false);
         view_delete.setId("view_delete");
-        name_area.setEditable(false);
-        pass_area.setEditable(false);
-        Separator frontSep = new Separator();
-        Separator backSep = new Separator();
         frontSep.setId("lineSeparator");
         backSep.setId("lineSeparator");
+
+        /*
+        Set up margin and editable for elements
+         */
+        user_area.setEditable(false);
+        name_area.setEditable(false);
+        pass_area.setEditable(false);
         frontSep.setOrientation(Orientation.VERTICAL);
         backSep.setOrientation(Orientation.VERTICAL);
         HBox.setMargin(frontSep,new Insets(-9,0,0,30));
@@ -37,22 +54,21 @@ public class LinePane extends HBox {
         HBox.setMargin(pass_area,new Insets(0,0,0,20));
         HBox.setMargin(view_delete,new Insets(0,0,0,10));
         view_delete.setPadding(new Insets(0,0,5,0));
-        //this.setPadding(new Insets(3,0,3,0));
         this.getStylesheets().add("CSS/View_css.css");
         this.getChildren().addAll(frontSep,name_area,user_area,pass_area,view_delete,backSep);
     }
     /*
-  Setter for elements in LinePane
- */
-    public void setName_area(String name) {
+        Create Setter for elements in LinePane
+    */
+    void setName_area(String name) {
         this.name_area.setText(name);
     }
 
-    public void setUser_area(String user) {
+    void setUser_area(String user) {
         this.user_area.setText(user);
     }
 
-    public void setPass_area(String pass) {
+    void setPass_area(String pass) {
         this.pass_area.setText(pass);
     }
 }
